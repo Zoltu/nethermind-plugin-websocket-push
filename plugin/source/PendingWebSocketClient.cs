8303590a3fde6eecba70ee9211d128a4f671cac5
Micah Zoltu <micah@zoltu.net>
2021-04-08 19:18:01 +0800

    Hacks in support for requesting receipts with blocks.
    By passing in a `0` contract address and a gas limit, you will now receive receipts for all pending transactions over the gas limit.

--- plugin/source/PendingWebSocketClient.cs
+++ plugin/source/PendingWebSocketClient.cs
@@ -24,6 +24,8 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 
 		private ImmutableArray<FilteredExecutionRequest> _filters = ImmutableArray<FilteredExecutionRequest>.Empty;
 
+		private Int64 _eventTracingGasLimit = Int64.MaxValue;
+
 		public PendingWebSocketClient(ILogger logger, IJsonSerializer nethermindJsonSerializer, IBlockTree blockTree, ITransactionProcessor transactionProcessor, IWebSocketPushConfig config, WebSocket webSocket, String id, String client) : base(logger, config, webSocket, id, client)
 		{
 			_nethermindJsonSerializer = nethermindJsonSerializer;
@@ -37,6 +39,11 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 			{
 				var dataString = System.Text.Encoding.UTF8.GetString(data.Span);
 				var filterOptions = _nethermindJsonSerializer.Deserialize<FilteredExecutionRequest>(dataString);
+				if (filterOptions.Contract == Address.Zero)
+				{
+					_eventTracingGasLimit = filterOptions.GasLimit;
+					return;
+				}
 				_filters = _filters.Add(filterOptions);
 				_logger.Info($"New Pending Filter added: {filterOptions.Contract}.{filterOptions.Signature}");
 				await Task.CompletedTask;
@@ -56,17 +63,20 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 		{
 			// get a local copy of the list we can work with for the duration of this method
 			var filters = _filters;
-			// if there are no filters, just send the pending transactions as they are (no execution details)
-			if (filters.IsEmpty)
+			if (transaction.GasLimit < _eventTracingGasLimit)
 			{
-				await SendSimpleTransaction(transaction);
-				return;
-			}
-			// if the gas limit for the transaction is less than the gas limit for all of the filters then ignore this transaction
-			if (filters.All(filter => transaction.GasLimit < filter.GasLimit))
-			{
-				await SendSimpleTransaction(transaction);
-				return;
+				// if there are no filters, just send the pending transactions as they are (no execution details)
+				if (filters.IsEmpty)
+				{
+					await SendSimpleTransaction(transaction);
+					return;
+				}
+				// if the gas limit for the transaction is less than the gas limit for all of the filters then ignore this transaction
+				if (filters.All(filter => transaction.GasLimit < filter.GasLimit))
+				{
+					await SendSimpleTransaction(transaction);
+					return;
+				}
 			}
 			var tracer = new Tracer(filters);
 			await Task.Run(() =>
@@ -76,12 +86,12 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 				var blockHeader = new BlockHeader(head.Hash!, Keccak.EmptyTreeHash, Address.Zero, head.Difficulty, head.Number + 1, head.GasLimit, head.Timestamp + 1, Array.Empty<Byte>());
 				_transactionProcessor.CallAndRestore(transaction, blockHeader, tracer);
 			});
-			if (tracer.FilterMatches.IsEmpty)
+			if (tracer.FilterMatches.IsEmpty && transaction.GasLimit < _eventTracingGasLimit)
 			{
 				await SendSimpleTransaction(transaction);
 				return;
 			}
-			await SendFilterMatchTransaction(transaction, tracer.FilterMatches);
+			await SendFilterMatchTransaction(transaction, tracer.FilterMatches, tracer.Logs);
 		}
 
 		private async Task SendSimpleTransaction(Transaction transaction)
@@ -90,9 +100,9 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 			await SendRawAsync(transactionAsString);
 		}
 
-		private async Task SendFilterMatchTransaction(Transaction transaction, ImmutableArray<FilterMatch> matches)
+		private async Task SendFilterMatchTransaction(Transaction transaction, ImmutableArray<FilterMatch> matches, ImmutableArray<LogEntry> logs)
 		{
-			var transactionAsString = _nethermindJsonSerializer.Serialize(new FilterMatchMessage(transaction, matches));
+			var transactionAsString = _nethermindJsonSerializer.Serialize(new FilterMatchMessage(transaction, matches, logs));
 			await SendRawAsync(transactionAsString);
 		}
 
@@ -102,16 +112,17 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 
 			public readonly UInt32 Signature { get; }
 
-			public readonly UInt256 GasLimit { get; }
+			public readonly Int64 GasLimit { get; }
 
-			public FilteredExecutionRequest(Address contract, UInt32 signature, UInt256 gasLimit) => (Contract, Signature, GasLimit) = (contract, signature, gasLimit);
+			public FilteredExecutionRequest(Address contract, UInt32 signature, Int64 gasLimit) => (Contract, Signature, GasLimit) = (contract, signature, gasLimit);
 		}
 
 		public readonly struct FilterMatchMessage
 		{
 			public readonly Transaction Transaction;
 			public readonly ImmutableArray<FilterMatch> FilterMatches;
-			public FilterMatchMessage(Transaction transaction, ImmutableArray<FilterMatch> filterMatches) => (Transaction, FilterMatches) = (transaction, filterMatches);
+			public readonly ImmutableArray<LogEntry> Logs;
+			public FilterMatchMessage(Transaction transaction, ImmutableArray<FilterMatch> filterMatches, ImmutableArray<LogEntry> logs) => (Transaction, FilterMatches, Logs) = (transaction, filterMatches, logs);
 		}
 
 		public readonly struct FilterMatch
@@ -137,10 +148,11 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 		{
 			private readonly ImmutableArray<FilteredExecutionRequest> _filters;
 			public ImmutableArray<FilterMatch> FilterMatches { get; private set; } = ImmutableArray<FilterMatch>.Empty;
+			public ImmutableArray<LogEntry> Logs { get; private set; } = ImmutableArray<LogEntry>.Empty;
 
 			public Tracer(ImmutableArray<FilteredExecutionRequest> filters) => _filters = filters;
 
-			public Boolean IsTracingReceipt => false;
+			public Boolean IsTracingReceipt => true;
 
 			public Boolean IsTracingActions => true;
 
@@ -163,7 +175,10 @@ namespace Zoltu.Nethermind.Plugin.WebSocketPush
 			public Boolean IsTracingStorage => throw new NotImplementedException();
 
 			public void MarkAsFailed(Address recipient, Int64 gasSpent, Byte[] output, String error, Keccak? stateRoot) { }
-			public void MarkAsSuccess(Address recipient, Int64 gasSpent, Byte[] output, LogEntry[] logs, Keccak? stateRoot) { }
+			public void MarkAsSuccess(Address recipient, Int64 gasSpent, Byte[] output, LogEntry[] logs, Keccak? stateRoot)
+			{
+				Logs = logs.ToImmutableArray();
+			}
 			public void ReportAccountRead(Address address) { }
 			public void ReportAction(Int64 gas, UInt256 value, Address from, Address to, Byte[] input, ExecutionType callType, Boolean isPrecompileCall)
 			{
